e2d55a64424a9df370b49560d69af599792e973b
md-amir-gauhar <gauharamir15@gmail.com>
2023-07-16 23:53:23 +0530

    added multiple elements of same type and handled test using getByRole options

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,8 +4,28 @@ import Application from "./application";
 describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const pageHeading = screen.getByRole("heading", {
+      name: "Job Application Form",
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      name: "Section 1",
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -2,37 +2,45 @@ import React from "react";
 
 const Application = () => {
   return (
-    <form
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "10px",
-        marginTop: "10px",
-      }}
-    >
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job location</label>
-        <select id="jon-location">
-          <option value="">Select a country</option>
-          <option value="US">United State</option>
-          <option value="UK">United Kingdom</option>
-          <option value="IN">India</option>
-          <option value="CA">Canada</option>
-          <option value="AUS">Australia</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" id="terms" /> I agree to the terms and
-          conditions
-        </label>
-      </div>
-      <button style={{ width: "fit-content" }}>Submit</button>
-    </form>
+    <div>
+      <h1>Job Application Form</h1>
+      <h2>Section 1</h2>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          marginTop: "10px",
+        }}
+      >
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" cols={10} />
+        </div>
+        <div>
+          <label htmlFor="job-location">Job location</label>
+          <select id="jon-location">
+            <option value="">Select a country</option>
+            <option value="US">United State</option>
+            <option value="UK">United Kingdom</option>
+            <option value="IN">India</option>
+            <option value="CA">Canada</option>
+            <option value="AUS">Australia</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> I agree to the terms and
+            conditions
+          </label>
+        </div>
+        <button style={{ width: "fit-content" }}>Submit</button>
+      </form>
+    </div>
   );
 };
 
